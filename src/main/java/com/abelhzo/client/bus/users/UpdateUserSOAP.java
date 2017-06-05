b0cee3f2525f0c4648520217c9ea480c94e4b1c4
AbelHZO <abel26.hernandez@gmail.com>
2017-06-04 23:34:06 -0500

    Colocacion de @XmlRootElement

--- src/main/java/com/abelhzo/client/bus/users/UpdateUserSOAP.java
+++ src/main/java/com/abelhzo/client/bus/users/UpdateUserSOAP.java
@@ -4,6 +4,7 @@ package com.abelhzo.client.bus.users;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import com.abelhzo.client.bus.commons.ConfigJAX;
 
@@ -28,6 +29,7 @@ import com.abelhzo.client.bus.commons.ConfigJAX;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "updateUserSOAP", propOrder = {
     "configJAX",
